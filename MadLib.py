09ef0ae641c023661a4b8d303255950671300648
mcamdin <mcguigancamdin@gmail.com>
2024-08-28 16:47:27 +0000

    final drafdt mad lib

--- MadLib.py
+++ MadLib.py
@@ -6,15 +6,15 @@
 def main():
   print("Madlib")
   #Ask user for words
-  noun1 = input("car")
-  name1 = input ("Aaron")
-  noun2 = input ("supermarket")
-  name2 = input ("Joe")
-  verb1 = input ("slipped")
-  verb2 = input ("dropping")
+  noun1 = input("enter a noun: ")
+  name1 = input ("enter a name: ")
+  noun2 = input ("enter a noun: ")
+  name2 = input ("enter a name: ")
+  verb1 = input ("enter a verb: ")
+  verb2 = input ("enter a verb: ")
   #Print the story with the user supplied words.
-  print("me and my friend" + name1 + "drove in a" + noun1 + "to the" + noun2 + "and saw our friend" + name2)
-  print (name2 + verb1 + "and fell on his back after" + verb2 + "the millk")
+  print("me and my friend " + name1 + " drove in a " + noun1 + " to the " + noun2 + " and saw our friend " + name2)
+  print (name2 + verb1 + " and fell on his back after " + verb2 + " the millk ")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
