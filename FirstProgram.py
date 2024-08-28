805a065206aae940831ef75e7ce4d22752fe4ac4
mcamdin <mcguigancamdin@gmail.com>
2024-08-28 16:42:30 +0000

    mad lib draft

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,24 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Camdin Wagner Mcguigan
+#Date: 08/28/2024
+#Assignment: Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  Age = input ("How old are you? ")
   #Tell the user what year they were born in.
+  Age = int(Age)
+  birthYear = 2024 - Age
+  print(birthYear)
   #Assume that they have not had their birthday yet this year.
-
-
+  print ("bithYear")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -1,16 +1,20 @@
 #MadLib.py
-#Name:
+#Name: Camdin Wagner McGuigan
 #Date:
 #Assignment:
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("car")
+  name1 = input ("Aaron")
+  noun2 = input ("supermarket")
+  name2 = input ("Joe")
+  verb1 = input ("slipped")
+  verb2 = input ("dropping")
   #Print the story with the user supplied words.
-
-
-
+  print("me and my friend" + name1 + "drove in a" + noun1 + "to the" + noun2 + "and saw our friend" + name2)
+  print (name2 + verb1 + "and fell on his back after" + verb2 + "the millk")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
